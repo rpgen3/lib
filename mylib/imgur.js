e15d8fd40ca035a117c995cd6635ec42fa3a8905
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-03-11 15:39:04 +0900

    Create imgur.js

---
+++ mylib/imgur.js
@@ -0,0 +1,51 @@
+(function(window, undefined){
+    "use strict";
+    function load(imgurID){
+        return new Promise(function(resolve, reject){
+            $("<img>").on("error", reject).on("load", function(){
+                resolve(this);
+            }).attr({
+                crossOrigin: "anonymous",
+                src: `https://i.imgur.com/${imgurID}.png`
+            });
+        });
+    }
+    function upload(dataURL){
+        const token = 'Client-ID ' + rpgen3.randArray(["ed3688de8608b9d","8b35a3e16a802a6","9f6cbdf697dab0b"]);
+        return new Promise(function(resolve, reject){
+            $.ajax({
+                dataType: 'json',
+                headers: {
+                    Authorization: token
+                },
+                url: "https://api.imgur.com/3/upload.json",
+                type: "POST",
+                data: {
+                    image: dataURL
+                },
+                success: function(r){
+                    const d = r.data;
+                    resolve(d.id, function(){ deleteFile(token, d.deletehash)});
+                },
+                error: reject
+            });
+        });
+    }
+    function deleteFile(token, dhash){
+        return new Promise(function(resolve, reject){
+            $.ajax({
+                dataType: 'json',
+                headers: {
+                    Authorization: token
+                },
+                url: "https://api.imgur.com/3/image/" + dhash,
+                type: "DELETE",
+                success: resolve
+            });
+        });
+    }
+    window.imgur = {
+        load: load,
+        upload: upload
+    };
+})(typeof window === 'object' ? window : this);
