95e8e5a80b3ce274e99c4438c47c124de4823290
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-04-26 11:51:17 +0900

    Update gradation.js

--- mylib/gradation.js
+++ mylib/gradation.js
@@ -3,23 +3,23 @@ function gradation(value, cycle){
           unit = cycle / 6,
           rate2 = value % unit / unit;
     let [r, g, b] = [255, 255, 255];
-    if(rate < 1/6){
+    if(rate <= 1/6){
         g *= rate2;
         b = 0;
     }
-    else if(rate < 2/6){
+    else if(rate <= 2/6){
         r = 255 - rate2 * r;
         b = 0;
     }
-    else if(rate < 3/6){
+    else if(rate <= 3/6){
         r = 0;
         b *= rate2;
     }
-    else if(rate < 4/6){
+    else if(rate <= 4/6){
         r = 0;
         g = 255 - rate2 * g;
     }
-    else if(rate < 5/6){
+    else if(rate <= 5/6){
         g = 0;
         r *= rate2;
     }
