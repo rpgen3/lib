2ba79816690667a7bca8be0ac38c479e95839be8
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-05-18 14:15:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 (async()=>{
     await import('https://rpgen3.github.io/lib/lib/jquery-3.5.1.min.js');
-    const rpgen3 = await import('https://rpgen3.github.io/lib/mylib/export/random.mjs');
-    const { randInt, randArr, shuffle } = await import('https://rpgen3.github.io/lib/mylib/export/random.mjs');
+    const rpgen3 = await(await import("https://rpgen3.github.io/lib/mylib/export/import.mjs")).default([
+        'baseN',
+        'css',
+        'hankaku',
+        'random',
+        'save',
+        'url',
+        'useful'
+    ]);
+    window.rpgen3 = rpgen3;
     const h = $("<div>").appendTo($("body")).css({
         "text-align": "center",
         padding: "1em"
     });
     $("<h1>").appendTo(h).text("ES2020 dynamic importでライブラリを作る");
+    $("<button>").appendTo(h).text("randInt").on("click",()=>{
+        msg(rpgen3.randInt(0,100));
+    });
     const hMsg = $("<div>").appendTo(h);
     function msg(str, isError){
         $("<span>").appendTo(hMsg.empty()).text(str).css({
@@ -14,7 +25,4 @@
             backgroundColor: isError ? 'pink' : 'lightblue'
         })
     }
-    $("<button>").appendTo(h).text("randInt").on("click",()=>{
-        msg(randInt(0,100));
-    });
 })();
