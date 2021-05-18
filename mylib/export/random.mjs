5a57ef11def0f8182727549172672d982f8f470f
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-05-18 09:47:18 +0900

    Update random.mjs

--- mylib/export/random.mjs
+++ mylib/export/random.mjs
@@ -1,10 +1,10 @@
-const _r = max => Math.floor(Math.random()*max);
-export const randInt = (min, max) => _r(Math.abs(max - min + 1)) + min;
-export const randArr = arr => arr[_r(arr.length)];
+const _f = max => Math.floor(Math.random() * max);
+export const randInt = (min, max) => _f(Math.abs(max - min + 1)) + min;
+export const randArr = arr => arr[_f(arr.length)];
 export const shuffle = arr => {
     let m = arr.length;
     while (m) {
-        const i = _r(m--);
+        const i = _f(m--);
         [arr[m], arr[i]] = [arr[i], arr[m]];
     }
     return arr;
