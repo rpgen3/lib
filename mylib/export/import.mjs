b1dfcae7462aadbaa5d2b0b046a62d9aa80b3228
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-05-18 14:30:27 +0900

    Update import.mjs

--- mylib/export/import.mjs
+++ mylib/export/import.mjs
@@ -1,2 +1,10 @@
-export default arr => Promise.all(arr.map(v=>import(`https://rpgen3.github.io/lib/mylib/export/${v}.mjs`))).then(v=>Object.assign({},...v));
-
+export const select = arr => Promise.all(arr.map(v=>import(`https://rpgen3.github.io/lib/mylib/export/${v}.mjs`))).then(v=>Object.assign({},...v));
+export const all = async () => await select([
+    'baseN',
+    'css',
+    'hankaku',
+    'random',
+    'save',
+    'url',
+    'useful'
+]);
